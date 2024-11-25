71f6c159b863e90ce9874d7d369dacd4b924d74d
horiaomar25 <horiaomar6@gmail.com>
2024-11-25 20:28:23 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /* ----------------- CONTROL FLOW EXERCISES ---------*/
+
+        /*---------------------------------------- TASK 1 ---------------------------------------*/
+        /*
+         * INSTRUCTIONS: Write a Java program using if-else statement to check if a number is a positive, negative, or 0.
+         * */
+
+        /*
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int num = reader.nextInt();
+
+        if(num > 0){
+            System.out.println("This number is positive");
+        } else if(num <  0){
+            System.out.println("This number is negative");
+        } else {
+            System.out.println("This number is zero");
+        }
+
+        */
+
+        /*---------------------------------------- TASK 2 ----------------------------------------*/
+
+        /*
+         * INSTRUCTIONS: Write a Java program using a switch statement to print the name of the day base on the day number.
+         * */
+
+       /*
+
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number and find out the day:");
+        int num = sc.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("It is Monday");
+                break;
+            case 2:
+                System.out.println("It is Tuesday");
+                break;
+            case 3:
+                System.out.println("It is Wednesday");
+                break;
+            case 4:
+                System.out.println("It is Thursday");
+                break;
+            case 5:
+                System.out.println("It is Friday");
+                break;
+            case 6:
+                System.out.println("It is Saturday");
+                break;
+            case 7:
+                System.out.println("It is Sunday");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
+        */
+
+        /*------------------------------------ TASK 3 -------------------------------------------- */
+
+
+
+
+    }
+}
